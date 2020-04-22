7200049557bfa390c4d0a3b985ff3c58e5f9cf6b
saurabhpathak072 <saurabhspathak072@gmail.com>
2020-04-22 15:43:43 +0530

    error block

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 let searchval;
 let search1;
+let theme;
 let NewsContainer = document.querySelector('.newsContainer1');
 let articles = [];
 let search =document.getElementById("search");
@@ -8,11 +9,14 @@ let pagenum = document.querySelectorAll(".page-item");
 let info = document.querySelector(".is-info");
 let page;
 let error = document.querySelector(".error");
+const switcher = document.getElementById('mode-switch');
+let cardd = document.getElementsByClassName('cardd');
 const headlinesFilter = {
     page: 1,
     pageSize: 20,
     q: ''
   };
+  let theme1= document.getElementById('theme');
 
 
 let getHeadLines = (searchval) =>{
@@ -39,7 +43,8 @@ let renderHeadlines =async (data) =>{
            NewsContainer.style.display='flex';
            info.style.display = 'none';
            error.style.display='none';
-            NewsList += `<li class="card-body card1"><h5 class="card-title arttile">${art.title}</h5>
+            NewsList += `
+                        <li class="card-body card1 "><h5 class="card-title arttile">${art.title}</h5>
             
                         <img class="img-thumbnail mx-auto d-block" style="height: 200px,text-align:center,
                         width: 200px;" src="${art.urlToImage}" alt="Card image cap">
@@ -59,7 +64,7 @@ let renderHeadlines =async (data) =>{
            NewsContainer.innerHTML=NewsList;
             console.log("Newscon",NewsContainer.innerHTML)
     }
-    else if(data.length==0){
+    else if(articles.length==0 && data.status==='ok'){
         console.log(info);
         info.style.display = 'block';
         console.log(NewsContainer);
@@ -68,7 +73,7 @@ let renderHeadlines =async (data) =>{
         
         
     }
-    else{
+    else if (data.status!=='ok'){
             console.log(error);
             error.style.display='block';
             NewsContainer.style.display='none';
@@ -95,4 +100,34 @@ let renderHeadlines =async (data) =>{
 }
 
 getHeadLines();
+switcher.addEventListener('change', e => {
+    const checked = e.target.checked;
+    const theme = checked ? 'dark' : 'light';
+    changeTheme(theme);
+  });
+
+  function changeTheme(theme) {
+    if (theme === 'dark') {
+      theme1.classList.add('wrapper');
+      theme1.classList.remove('nowrapper');
+      localStorage.setItem('theme', 'dark');
+     
+
+    } else if (theme === 'light') {
+      theme1.classList.remove('wrapper');
+      theme1.classList.add('nowrapper');
+      localStorage.setItem('theme', 'light');
+      
+    }
+  }
+  function themeInit() {
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  } else {
+    theme = 'light';
+    localStorage.setItem('theme', 'light');
+  }
+  changeTheme(theme);
+  switcher.checked = theme === 'dark' ? true : false;
+}
 
